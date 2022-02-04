10bbafa54d812201b749691f289327a8d04a1b02
Loris Leiva <loris.leiva@gmail.com>
2022-02-04 11:28:03 +0000

    wip

---
+++ src/useWallet.ts
@@ -0,0 +1,13 @@
+import { WalletNotInitializedError } from './errors';
+import { createWalletStore, WalletStore, WalletStoreProps } from './createWalletStore';
+
+let walletStore: WalletStore | null = null;
+
+export const useWallet = (): WalletStore => {
+  if (walletStore) return walletStore;
+  throw new WalletNotInitializedError('Wallet not initialized. Please use the `initWallet` method to initialize the wallet.');
+};
+
+export const initWallet = (walletStoreProps: WalletStoreProps): void => {
+  walletStore = createWalletStore(walletStoreProps);
+};
