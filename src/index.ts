e6b48d16c44deb3b3db9ac63a5ab1eedbdf48ff7
Loris Leiva <loris.leiva@gmail.com>
2022-02-04 11:22:53 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { ref, Ref } from 'vue-demi'
-
-type MagicNumber = number;
-
-export const foo: MagicNumber = 3;
-
-export const bar: Ref<MagicNumber> = ref(42);
+export * from './createWalletStore';
+export * from './errors';
+export * from './useLocalStorage';
