ba1f6fdef0ca2c84690a436896b6e7fde15b6245
Loris Leiva <loris.leiva@gmail.com>
2022-02-04 14:44:35 +0000

    wip

--- example/App.vue
+++ example/App.vue
@@ -1,6 +1,6 @@
 <script>
 import { computed } from 'vue-demi'
-import { Connection, PublicKey, Keypair } from '@solana/web3.js'
+import { Connection, PublicKey, Keypair, clusterApiUrl } from '@solana/web3.js'
 import { Program, Provider } from '@project-serum/anchor'
 import { WalletModalProvider, WalletMultiButton, useAnchorWallet } from '../src'
 import idl from './idl.json'
@@ -9,7 +9,7 @@ const programID = new PublicKey(idl.metadata.address)
 const preflightCommitment = 'processed'
 const baseAccount = Keypair.generate()
 const wallet = useAnchorWallet()
-const connection = new Connection(network, preflightCommitment)
+const connection = new Connection(clusterApiUrl('devnet'), preflightCommitment)
 const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment }))
 const program = computed(() => new Program(idl, programID, provider.value))
 
