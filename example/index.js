eb594a505ced85495a12805bc3c598a1be50c052
Loris Leiva <loris.leiva@gmail.com>
2022-02-04 13:16:11 +0000

    wip

--- example/index.js
+++ example/index.js
@@ -2,8 +2,26 @@ import { createApp } from 'vue-demi';
 import App from './App.vue';
 import SolanaWallets from '../src/index';
 
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+
 const walletOptions = {
-  wallets: [],
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter({ network: 'devnet' }),
+    new TorusWalletAdapter(),
+    new LedgerWalletAdapter(),
+    new SolletWalletAdapter({ network: 'devnet' }),
+    new SolletExtensionWalletAdapter({ network: 'devnet' }),
+  ],
   autoConnect: true,
 }
 
