3719ea7d31aa90927f4f7d10a8fcd6825f9caae4
Loris Leiva <loris.leiva@gmail.com>
2022-02-04 13:16:50 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     rollupOptions: {
       external: ['vue'],
       input: {
+        index: path.resolve(__dirname, 'src/index.html'),
         example: path.resolve(__dirname, 'example/index.html'),
       },
       output: {
