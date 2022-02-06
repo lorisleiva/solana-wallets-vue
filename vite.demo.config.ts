ad30c8e1a0ef579198daa0dd00d0d6b970c185c7
Loris Leiva <loris.leiva@gmail.com>
2022-02-06 14:27:07 +0000

    wip

---
+++ vite.demo.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const path = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  root: path.resolve(__dirname, 'example'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  define: {
+    'process.env': {}
+  },
+})
