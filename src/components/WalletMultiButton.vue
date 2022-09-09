c5aa6e68e8e3a3b5cdb15d343916943fbdd8b89f
Rob <robaanhane@gmail.com>
2022-09-09 16:58:13 +0200

    fix: also clear storage when clicking on disconnect dropdown button

--- src/components/WalletMultiButton.vue
+++ src/components/WalletMultiButton.vue
@@ -1,10 +1,10 @@
 <script lang="ts">
-import { computed, defineComponent, ref, toRefs } from "vue";
-import { onClickOutside, useClipboard } from "@vueuse/core";
-import { useWallet } from "@/useWallet";
-import WalletConnectButton from "./WalletConnectButton.vue";
-import WalletIcon from "./WalletIcon.vue";
-import WalletModalProvider from "./WalletModalProvider.vue";
+import { computed, defineComponent, ref, toRefs } from 'vue';
+import { onClickOutside, useClipboard } from '@vueuse/core';
+import { useWallet } from '@/useWallet';
+import WalletConnectButton from './WalletConnectButton.vue';
+import WalletIcon from './WalletIcon.vue';
+import WalletModalProvider from './WalletModalProvider.vue';
 
 export default defineComponent({
   components: {
@@ -31,11 +31,20 @@ export default defineComponent({
     const publicKeyBase58 = computed(() => publicKey.value?.toBase58());
     const publicKeyTrimmed = computed(() => {
       if (!wallet.value || !publicKeyBase58.value) return null;
-      return publicKeyBase58.value.slice(0, 4) + ".." + publicKeyBase58.value.slice(-4);
+      return (
+        publicKeyBase58.value.slice(0, 4) +
+        '..' +
+        publicKeyBase58.value.slice(-4)
+      );
     });
 
-    const { copy, copied: addressCopied, isSupported: canCopy } = useClipboard()
-    const copyAddress = () => publicKeyBase58.value && copy(publicKeyBase58.value);
+    const {
+      copy,
+      copied: addressCopied,
+      isSupported: canCopy,
+    } = useClipboard();
+    const copyAddress = () =>
+      publicKeyBase58.value && copy(publicKeyBase58.value);
 
     // Define the bindings given to scoped slots.
     const scope = {
@@ -55,7 +64,7 @@ export default defineComponent({
       closeDropdown,
       copyAddress,
       disconnect,
-    }
+    };
 
     return {
       scope,
@@ -66,13 +75,24 @@ export default defineComponent({
 </script>
 
 <template>
-  <wallet-modal-provider :featured="featured" :container="container" :logo="logo" :dark="dark">
+  <wallet-modal-provider
+    :featured="featured"
+    :container="container"
+    :logo="logo"
+    :dark="dark"
+  >
     <template #default="modalScope">
       <slot v-bind="{ ...modalScope, ...scope }">
-        <button v-if="!wallet" class="swv-button swv-button-trigger" @click="modalScope.openModal">
+        <button
+          v-if="!wallet"
+          class="swv-button swv-button-trigger"
+          @click="modalScope.openModal"
+        >
           Select Wallet
         </button>
-        <wallet-connect-button v-else-if="!publicKeyBase58"></wallet-connect-button>
+        <wallet-connect-button
+          v-else-if="!publicKeyBase58"
+        ></wallet-connect-button>
         <div v-else class="swv-dropdown">
           <slot name="dropdown-button" v-bind="{ ...modalScope, ...scope }">
             <button
@@ -95,13 +115,29 @@ export default defineComponent({
               role="menu"
             >
               <slot name="dropdown-list" v-bind="{ ...modalScope, ...scope }">
-                <li v-if="canCopy" @click="copyAddress" class="swv-dropdown-list-item" role="menuitem">
-                  {{ addressCopied ? "Copied" : "Copy address" }}
+                <li
+                  v-if="canCopy"
+                  @click="copyAddress"
+                  class="swv-dropdown-list-item"
+                  role="menuitem"
+                >
+                  {{ addressCopied ? 'Copied' : 'Copy address' }}
                 </li>
-                <li @click="modalScope.openModal(); closeDropdown();" class="swv-dropdown-list-item" role="menuitem">
+                <li
+                  @click="
+                    modalScope.openModal();
+                    closeDropdown();
+                  "
+                  class="swv-dropdown-list-item"
+                  role="menuitem"
+                >
                   Change wallet
                 </li>
-                <li @click="disconnect" class="swv-dropdown-list-item" role="menuitem">
+                <li
+                  @click="disconnect(true)"
+                  class="swv-dropdown-list-item"
+                  role="menuitem"
+                >
                   Disconnect
                 </li>
               </slot>
