37c6b5f0a2eb8af429cf52c38aca5ba387428b70
Rob <robaanhane@gmail.com>
2022-09-09 11:10:18 +0200

    chore: update to latest version of typescript

--- src/components/WalletModalProvider.vue
+++ src/components/WalletModalProvider.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
-import { computed, defineComponent, nextTick, ref, toRefs, watch } from "vue";
-import { onClickOutside, onKeyStroke, useScrollLock } from "@vueuse/core";
-import { useWallet } from "@/useWallet";
+import { computed, defineComponent, nextTick, ref, toRefs, watch } from 'vue';
+import { onClickOutside, onKeyStroke, useScrollLock } from '@vueuse/core';
+import { useWallet } from '@/useWallet';
 import WalletIcon from './WalletIcon.vue';
 
 export default defineComponent({
@@ -18,44 +18,59 @@ export default defineComponent({
     const { featured, container, logo, dark } = toRefs(props);
     const modalPanel = ref<HTMLElement | null>(null);
     const modalOpened = ref(false);
-    const openModal = () => modalOpened.value = true;
-    const closeModal = () => modalOpened.value = false;
-    const hasLogo = computed(() => !!slots.logo || !!logo.value)
+    const openModal = () => (modalOpened.value = true);
+    const closeModal = () => (modalOpened.value = false);
+    const hasLogo = computed(() => !!slots.logo || !!logo.value);
 
     const { wallets, select: selectWallet } = useWallet();
     const expandedWallets = ref(false);
-    const featuredWallets = computed(() => wallets.value.slice(0, featured.value));
+    const featuredWallets = computed(() =>
+      wallets.value.slice(0, featured.value)
+    );
     const hiddenWallets = computed(() => wallets.value.slice(featured.value));
-    const walletsToDisplay = computed(() => expandedWallets.value ? wallets.value : featuredWallets.value)
+    const walletsToDisplay = computed(() =>
+      expandedWallets.value ? wallets.value : featuredWallets.value
+    );
 
     // Close the modal when clicking outside of it or when pressing Escape.
-    onClickOutside(modalPanel, closeModal);
-    onKeyStroke("Escape", closeModal);
+    onClickOutside(modalPanel as any, closeModal);
+    onKeyStroke('Escape', closeModal);
 
     // Ensures pressing Tab backwards and forwards stays within the modal.
-    onKeyStroke("Tab", (event: KeyboardEvent) => {
-      const focusableElements = modalPanel.value?.querySelectorAll("button") ?? [];
+    onKeyStroke('Tab', (event: KeyboardEvent) => {
+      const focusableElements =
+        modalPanel.value?.querySelectorAll('button') ?? [];
       const firstElement = focusableElements?.[0];
       const lastElement = focusableElements?.[focusableElements.length - 1];
 
-      if (event.shiftKey && document.activeElement === firstElement && lastElement) {
+      if (
+        event.shiftKey &&
+        document.activeElement === firstElement &&
+        lastElement
+      ) {
         lastElement.focus();
         event.preventDefault();
-      } else if (! event.shiftKey && document.activeElement === lastElement && firstElement) {
+      } else if (
+        !event.shiftKey &&
+        document.activeElement === lastElement &&
+        firstElement
+      ) {
         firstElement.focus();
         event.preventDefault();
       }
     });
 
     // Bring focus inside the modal when it opens.
-    watch(modalOpened, isOpened => {
-      if (! isOpened) return;
-      nextTick(() => modalPanel.value?.querySelectorAll("button")?.[0]?.focus())
+    watch(modalOpened, (isOpened) => {
+      if (!isOpened) return;
+      nextTick(() =>
+        modalPanel.value?.querySelectorAll('button')?.[0]?.focus()
+      );
     });
 
     // Lock the body scroll when the modal opens.
     const scrollLock = useScrollLock(document.body);
-    watch(modalOpened, isOpened => scrollLock.value = isOpened);
+    watch(modalOpened, (isOpened) => (scrollLock.value = isOpened));
 
     // Define the bindings given to scoped slots.
     const scope = {
@@ -64,7 +79,7 @@ export default defineComponent({
       hasLogo,
       featured,
       container,
-      modalPanel,
+      modalPanel: modalPanel as any, // To suppress TS7056
       modalOpened,
       openModal,
       closeModal,
@@ -73,12 +88,12 @@ export default defineComponent({
       featuredWallets,
       hiddenWallets,
       selectWallet,
-    }
+    };
 
     return {
       scope,
       ...scope,
-    }
+    };
   },
 });
 </script>
@@ -109,19 +124,22 @@ export default defineComponent({
                 <img alt="logo" class="swv-modal-logo" :src="logo" />
               </slot>
             </div>
-            <h1 class="swv-modal-title" id="swv-modal-title">
-              Connect Wallet
-            </h1>
+            <h1 class="swv-modal-title" id="swv-modal-title">Connect Wallet</h1>
             <button @click.prevent="closeModal" class="swv-modal-button-close">
               <svg width="14" height="14">
-                <path d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z" />
+                <path
+                  d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z"
+                />
               </svg>
             </button>
             <ul class="swv-modal-list">
               <li
                 v-for="wallet in walletsToDisplay"
                 :key="wallet.name"
-                @click="selectWallet(wallet.name); closeModal();"
+                @click="
+                  selectWallet(wallet.name);
+                  closeModal();
+                "
               >
                 <button class="swv-button">
                   <p v-text="wallet.name"></p>
@@ -137,10 +155,12 @@ export default defineComponent({
               :class="{ 'swv-modal-collapse-button-active': expandedWallets }"
               @click="expandedWallets = !expandedWallets"
             >
-              {{ expandedWallets ? "Less" : "More" }} options
+              {{ expandedWallets ? 'Less' : 'More' }} options
               <i class="swv-button-icon">
                 <svg width="11" height="6" xmlns="http://www.w3.org/2000/svg">
-                  <path d="m5.938 5.73 4.28-4.126a.915.915 0 0 0 0-1.322 1 1 0 0 0-1.371 0L5.253 3.736 1.659.272a1 1 0 0 0-1.371 0A.93.93 0 0 0 0 .932c0 .246.1.48.288.662l4.28 4.125a.99.99 0 0 0 1.37.01z" />
+                  <path
+                    d="m5.938 5.73 4.28-4.126a.915.915 0 0 0 0-1.322 1 1 0 0 0-1.371 0L5.253 3.736 1.659.272a1 1 0 0 0-1.371 0A.93.93 0 0 0 0 .932c0 .246.1.48.288.662l4.28 4.125a.99.99 0 0 0 1.37.01z"
+                  />
                 </svg>
               </i>
             </button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   define: {
-    'process.env': {}
+    'process.env': {},
   },
   build: {
     lib: {
@@ -33,4 +33,4 @@ export default defineConfig({
     sourcemap: true,
     minify: false,
   },
-})
+});
