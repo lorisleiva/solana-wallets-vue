d0f7a1085fb7cb6e8e61c129ceb12fb757f34208
Loris Leiva <loris.leiva@gmail.com>
2022-10-16 16:12:11 +0100

    chore: update components

--- src/components/WalletModalProvider.vue
+++ src/components/WalletModalProvider.vue
@@ -2,7 +2,7 @@
 import { computed, defineComponent, nextTick, ref, toRefs, watch } from "vue";
 import { onClickOutside, onKeyStroke, useScrollLock } from "@vueuse/core";
 import { useWallet } from "@/useWallet";
-import WalletIcon from './WalletIcon.vue';
+import WalletIcon from "./WalletIcon.vue";
 
 export default defineComponent({
   components: {
@@ -10,7 +10,7 @@ export default defineComponent({
   },
   props: {
     featured: { type: Number, default: 3 },
-    container: { type: String, default: 'body' },
+    container: { type: String, default: "body" },
     logo: String,
     dark: Boolean,
   },
@@ -18,15 +18,19 @@ export default defineComponent({
     const { featured, container, logo, dark } = toRefs(props);
     const modalPanel = ref<HTMLElement | null>(null);
     const modalOpened = ref(false);
-    const openModal = () => modalOpened.value = true;
-    const closeModal = () => modalOpened.value = false;
-    const hasLogo = computed(() => !!slots.logo || !!logo.value)
+    const openModal = () => (modalOpened.value = true);
+    const closeModal = () => (modalOpened.value = false);
+    const hasLogo = computed(() => !!slots.logo || !!logo.value);
 
     const { wallets, select: selectWallet } = useWallet();
     const expandedWallets = ref(false);
-    const featuredWallets = computed(() => wallets.value.slice(0, featured.value));
+    const featuredWallets = computed(() =>
+      wallets.value.slice(0, featured.value)
+    );
     const hiddenWallets = computed(() => wallets.value.slice(featured.value));
-    const walletsToDisplay = computed(() => expandedWallets.value ? wallets.value : featuredWallets.value)
+    const walletsToDisplay = computed(() =>
+      expandedWallets.value ? wallets.value : featuredWallets.value
+    );
 
     // Close the modal when clicking outside of it or when pressing Escape.
     onClickOutside(modalPanel, closeModal);
@@ -34,28 +38,39 @@ export default defineComponent({
 
     // Ensures pressing Tab backwards and forwards stays within the modal.
     onKeyStroke("Tab", (event: KeyboardEvent) => {
-      const focusableElements = modalPanel.value?.querySelectorAll("button") ?? [];
+      const focusableElements =
+        modalPanel.value?.querySelectorAll("button") ?? [];
       const firstElement = focusableElements?.[0];
       const lastElement = focusableElements?.[focusableElements.length - 1];
 
-      if (event.shiftKey && document.activeElement === firstElement && lastElement) {
+      if (
+        event.shiftKey &&
+        document.activeElement === firstElement &&
+        lastElement
+      ) {
         lastElement.focus();
         event.preventDefault();
-      } else if (! event.shiftKey && document.activeElement === lastElement && firstElement) {
+      } else if (
+        !event.shiftKey &&
+        document.activeElement === lastElement &&
+        firstElement
+      ) {
         firstElement.focus();
         event.preventDefault();
       }
     });
 
     // Bring focus inside the modal when it opens.
-    watch(modalOpened, isOpened => {
-      if (! isOpened) return;
-      nextTick(() => modalPanel.value?.querySelectorAll("button")?.[0]?.focus())
+    watch(modalOpened, (isOpened) => {
+      if (!isOpened) return;
+      nextTick(() =>
+        modalPanel.value?.querySelectorAll("button")?.[0]?.focus()
+      );
     });
 
     // Lock the body scroll when the modal opens.
     const scrollLock = useScrollLock(document.body);
-    watch(modalOpened, isOpened => scrollLock.value = isOpened);
+    watch(modalOpened, (isOpened) => (scrollLock.value = isOpened));
 
     // Define the bindings given to scoped slots.
     const scope = {
@@ -73,12 +88,12 @@ export default defineComponent({
       featuredWallets,
       hiddenWallets,
       selectWallet,
-    }
+    };
 
     return {
       scope,
       ...scope,
-    }
+    };
   },
 });
 </script>
@@ -109,22 +124,25 @@ export default defineComponent({
                 <img alt="logo" class="swv-modal-logo" :src="logo" />
               </slot>
             </div>
-            <h1 class="swv-modal-title" id="swv-modal-title">
-              Connect Wallet
-            </h1>
+            <h1 class="swv-modal-title" id="swv-modal-title">Connect Wallet</h1>
             <button @click.prevent="closeModal" class="swv-modal-button-close">
               <svg width="14" height="14">
-                <path d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z" />
+                <path
+                  d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z"
+                />
               </svg>
             </button>
             <ul class="swv-modal-list">
               <li
                 v-for="wallet in walletsToDisplay"
-                :key="wallet.name"
-                @click="selectWallet(wallet.name); closeModal();"
+                :key="wallet.adapter.name"
+                @click="
+                  selectWallet(wallet.adapter.name);
+                  closeModal();
+                "
               >
                 <button class="swv-button">
-                  <p v-text="wallet.name"></p>
+                  <p v-text="wallet.adapter.name"></p>
                   <wallet-icon :wallet="wallet"></wallet-icon>
                 </button>
               </li>
@@ -140,7 +158,9 @@ export default defineComponent({
               {{ expandedWallets ? "Less" : "More" }} options
               <i class="swv-button-icon">
                 <svg width="11" height="6" xmlns="http://www.w3.org/2000/svg">
-                  <path d="m5.938 5.73 4.28-4.126a.915.915 0 0 0 0-1.322 1 1 0 0 0-1.371 0L5.253 3.736 1.659.272a1 1 0 0 0-1.371 0A.93.93 0 0 0 0 .932c0 .246.1.48.288.662l4.28 4.125a.99.99 0 0 0 1.37.01z" />
+                  <path
+                    d="m5.938 5.73 4.28-4.126a.915.915 0 0 0 0-1.322 1 1 0 0 0-1.371 0L5.253 3.736 1.659.272a1 1 0 0 0-1.371 0A.93.93 0 0 0 0 .932c0 .246.1.48.288.662l4.28 4.125a.99.99 0 0 0 1.37.01z"
+                  />
                 </svg>
               </i>
             </button>
