6f28872268befc8d76f1e29d1cb9e2e5d3f33274
fido1hn <confidoekeanya@gmail.com>
2025-01-31 02:49:17 +0100

    chore: update dependencies and ensure example works

--- example/App.vue
+++ example/App.vue
@@ -8,7 +8,7 @@ import {
   clusterApiUrl,
   SystemProgram,
 } from "@solana/web3.js";
-import { AnchorProvider, Program } from "@project-serum/anchor";
+import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import { WalletMultiButton, useAnchorWallet } from "../src";
 import idl from "./idl.json";
 
@@ -93,8 +93,7 @@ export default {
 <template>
   <div
     class="h-screen w-screen flex"
-    :class="dark ? 'bg-gray-900 text-gray-100' : 'bg-gray-100 text-gray-700'"
-  >
+    :class="dark ? 'bg-gray-900 text-gray-100' : 'bg-gray-100 text-gray-700'">
     <!-- Top-Right Corner. -->
     <div class="absolute top-0 right-0 p-8 flex space-x-8">
       <!-- Dark Button. -->
@@ -105,22 +104,19 @@ export default {
           dark
             ? 'bg-white/10 hover:bg-white/20 text-gray-200'
             : 'bg-black/10 hover:bg-black/20 text-gray-600'
-        "
-      >
+        ">
         <svg
           v-if="dark"
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
+          stroke="currentColor">
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
+            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
         <svg
           v-else
@@ -128,14 +124,12 @@ export default {
           class="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
+          stroke="currentColor">
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
       </button>
 
@@ -147,34 +141,29 @@ export default {
     <div class="m-auto w-full max-w-md p-8">
       <div
         class="shadow-xl rounded-xl"
-        :class="dark ? 'bg-gray-700' : 'bg-white'"
-      >
+        :class="dark ? 'bg-gray-700' : 'bg-white'">
         <div class="p-8 text-center">
           <p
-            class="uppercase text-xs tracking-widest text-gray-400 font-semibold"
-          >
+            class="uppercase text-xs tracking-widest text-gray-400 font-semibold">
             Counter
           </p>
           <p
             class="font-bold text-5xl mt-2"
             :class="dark ? 'text-white' : 'text-gray-900'"
-            v-text="counterPublicKey ? counter : 'Not Set'"
-          ></p>
+            v-text="counterPublicKey ? counter : 'Not Set'"></p>
         </div>
 
         <div class="flex">
           <button
             class="flex-1 py-4 px-2 rounded-bl-xl"
             :class="dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'"
-            @click="createCounter"
-          >
+            @click="createCounter">
             New Counter
           </button>
           <button
             class="flex-1 py-4 px-2 rounded-br-xl"
             :class="dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'"
-            @click="incrementCounter"
-          >
+            @click="incrementCounter">
             Increment Counter
           </button>
         </div>
--- example/index.js
+++ example/index.js
@@ -5,21 +5,15 @@ import "../styles.css";
 
 import {
   PhantomWalletAdapter,
-  SlopeWalletAdapter,
   SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
   TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 
 const walletOptions = {
   wallets: [
     new PhantomWalletAdapter(),
-    new SlopeWalletAdapter(),
     new SolflareWalletAdapter({ network: "devnet" }),
     new TorusWalletAdapter(),
-    new SolletWalletAdapter({ network: "devnet" }),
-    new SolletExtensionWalletAdapter({ network: "devnet" }),
   ],
   autoConnect: true,
 };
